f11ff02c8ad95aebdf016398daa7f0735ff95545
student <preethi_b@hcl.com>
2021-04-17 18:45:57 +0000

    initial commit

---
+++ MavenProject/src/main/java/com/hcl/MavenProject/App.java
@@ -0,0 +1,13 @@
+package com.hcl.MavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Dev-Ops");
+    }
+}
